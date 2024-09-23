8ad1c408a992b1b97eb738287455b1ce7248ca24
Juan <navarrojgr@gmail.com>
2024-09-23 10:23:41 -0400

    uploading footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+const footer = () => {
+    return (
+        <>
+        <footer> <p>This was created by juan n</p></footer>
+        </>
+    
+    )}
+
+export default footer
